49d7e6e1b3c933f8c22886fb9a62b38f5e1e9d75
bayukrs <bayukrismant@gmail.com>
2020-04-11 14:02:29 +0700

    add tugas kelompok database

---
+++ keyjob.sql
@@ -0,0 +1,283 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 10 Apr 2020 pada 15.41
+-- Versi Server: 10.1.28-MariaDB
+-- PHP Version: 7.1.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `keyjob`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(10) NOT NULL,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `freelancer`
+--
+
+CREATE TABLE `freelancer` (
+  `id_freelancer` int(10) UNSIGNED NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `freelancer`
+--
+
+INSERT INTO `freelancer` (`id_freelancer`, `nama`, `email`, `username`, `password`) VALUES
+(1, 'Bayu Krismanto', 'bayukrismant@gmail.com', 'bayukrismanto', '202cb962ac59075b964b07152d234b70'),
+(2, 'admin', 'bayukrismant@yahoo.com', 'admin', '21232f297a57a5a743894a0e4a801fc3'),
+(3, 'Budi', 'budi@gmail.com', 'budi', '202cb962ac59075b964b07152d234b70');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_detail_order`
+--
+
+CREATE TABLE `tbl_detail_order` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `order_id` int(10) DEFAULT NULL,
+  `produk` int(10) DEFAULT NULL,
+  `qty` int(10) DEFAULT NULL,
+  `harga` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_detail_order`
+--
+
+INSERT INTO `tbl_detail_order` (`id`, `order_id`, `produk`, `qty`, `harga`) VALUES
+(1, 1, 2, 2, '500000');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_kategori`
+--
+
+CREATE TABLE `tbl_kategori` (
+  `id_kategori` int(10) UNSIGNED NOT NULL,
+  `nama_kategori` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_kategori`
+--
+
+INSERT INTO `tbl_kategori` (`id_kategori`, `nama_kategori`) VALUES
+(1, 'Web and Programing'),
+(2, 'Desain Grafis'),
+(3, 'Translate');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_order`
+--
+
+CREATE TABLE `tbl_order` (
+  `id` int(10) NOT NULL,
+  `tanggal` date DEFAULT NULL,
+  `pelanggan` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_order`
+--
+
+INSERT INTO `tbl_order` (`id`, `tanggal`, `pelanggan`) VALUES
+(1, '2020-04-10', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_pelanggan`
+--
+
+CREATE TABLE `tbl_pelanggan` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `alamat` varchar(50) DEFAULT NULL,
+  `telp` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_pelanggan`
+--
+
+INSERT INTO `tbl_pelanggan` (`id`, `nama`, `email`, `alamat`, `telp`) VALUES
+(1, 'Bayu Krismanto', 'bayukrismant@gmail.com', 'Semarang', '089684290');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_produk`
+--
+
+CREATE TABLE `tbl_produk` (
+  `id_produk` int(10) UNSIGNED NOT NULL,
+  `id_kategori` int(10) UNSIGNED NOT NULL,
+  `id_freelancer` int(10) UNSIGNED NOT NULL,
+  `nama_produk` varchar(100) NOT NULL,
+  `harga` int(10) NOT NULL,
+  `gambar` varchar(50) NOT NULL,
+  `deskripsi` varchar(255) DEFAULT NULL,
+  `benefit` varchar(255) DEFAULT NULL,
+  `waktu_pengerjaan` int(2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_produk`
+--
+
+INSERT INTO `tbl_produk` (`id_produk`, `id_kategori`, `id_freelancer`, `nama_produk`, `harga`, `gambar`, `deskripsi`, `benefit`, `waktu_pengerjaan`) VALUES
+(3, 2, 3, 'Logo Design', 500000, 'Logobarudjp.jpg', 'Design your logo Company', '3x revisi', 3);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `freelancer`
+--
+ALTER TABLE `freelancer`
+  ADD PRIMARY KEY (`id_freelancer`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `tbl_detail_order`
+--
+ALTER TABLE `tbl_detail_order`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `tbl_order`
+--
+ALTER TABLE `tbl_order`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_pelanggan`
+--
+ALTER TABLE `tbl_pelanggan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_produk`
+--
+ALTER TABLE `tbl_produk`
+  ADD PRIMARY KEY (`id_produk`),
+  ADD KEY `fk1` (`id_kategori`),
+  ADD KEY `fk2` (`id_freelancer`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `freelancer`
+--
+ALTER TABLE `freelancer`
+  MODIFY `id_freelancer` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tbl_detail_order`
+--
+ALTER TABLE `tbl_detail_order`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  MODIFY `id_kategori` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tbl_order`
+--
+ALTER TABLE `tbl_order`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_pelanggan`
+--
+ALTER TABLE `tbl_pelanggan`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_produk`
+--
+ALTER TABLE `tbl_produk`
+  MODIFY `id_produk` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `tbl_produk`
+--
+ALTER TABLE `tbl_produk`
+  ADD CONSTRAINT `fk1` FOREIGN KEY (`id_kategori`) REFERENCES `tbl_kategori` (`id_kategori`),
+  ADD CONSTRAINT `fk2` FOREIGN KEY (`id_freelancer`) REFERENCES `freelancer` (`id_freelancer`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
